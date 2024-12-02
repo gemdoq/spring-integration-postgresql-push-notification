6dc7ed4d6a41deb4df78fc77626f5b1b9b40d755
SeungHo <gemdoq@gmail.com>
2024-12-02 14:29:04 +0900

    test(mail): SmtpMailSenderTest

---
+++ src/test/java/com/springboot/springintegrationpostgresqlpushnotification/global/util/mail/SmtpMailSenderTest.java
@@ -0,0 +1,25 @@
+package com.springboot.springintegrationpostgresqlpushnotification.global.util.mail;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+class SmtpMailSenderTest {
+
+	private final JavaMailSender javaMailSender = Mockito.mock(JavaMailSender.class);
+	private final SmtpMailSender smtpMailSender = new SmtpMailSender(javaMailSender);
+
+	@Test
+	@DisplayName("Is the outbox being saved correctly through the repository")
+	void testSendMail() {
+		MailMessage mailMessage = new MailMessage("Test Subject", "Test Body", "test@example.com");
+		smtpMailSender.sendMail(mailMessage);
+		verify(javaMailSender, times(1)).send(Mockito.any(SimpleMailMessage.class));
+	}
+}
