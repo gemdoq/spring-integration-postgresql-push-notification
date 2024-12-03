83cfcbdac63a0a9cd1067599971fa834a11ef5b7
SeungHo <gemdoq@gmail.com>
2024-12-04 00:20:25 +0900

    feat(mail): add service method and test to send emails using a template with variables substituted in html and

--- src/main/java/com/springboot/springintegrationpostgresqlpushnotification/domain/order/service/OrderService.java
+++ src/main/java/com/springboot/springintegrationpostgresqlpushnotification/domain/order/service/OrderService.java
@@ -8,10 +8,14 @@ import com.springboot.springintegrationpostgresqlpushnotification.domain.order.d
 import com.springboot.springintegrationpostgresqlpushnotification.domain.order.data.repository.OrderRepository;
 import com.springboot.springintegrationpostgresqlpushnotification.global.util.mail.MailGateway;
 import com.springboot.springintegrationpostgresqlpushnotification.global.util.mail.MailMessage;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.messaging.Message;
 import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
+import org.thymeleaf.context.Context;
+import org.thymeleaf.spring6.SpringTemplateEngine;
 
 import java.math.BigDecimal;
 import java.sql.Timestamp;
@@ -21,14 +25,20 @@ import java.util.UUID;
 @Component
 @Transactional
 public class OrderService {
+
+	@Value("${MAIL_USERNAME}")
+	private String MAIL_USERNAME;
+
 	private final OrderRepository orderRepository;
 	private final OutboxRepository outboxRepository;
 	private final MailGateway mailGateway;
+	private SpringTemplateEngine templateEngine;
 
-	public OrderService(OrderRepository orderRepository, OutboxRepository outboxRepository, MailGateway mailGateway) {
+	public OrderService(OrderRepository orderRepository, OutboxRepository outboxRepository, MailGateway mailGateway, SpringTemplateEngine templateEngine) {
 		this.orderRepository = orderRepository;
 		this.outboxRepository = outboxRepository;
 		this.mailGateway = mailGateway;
+		this.templateEngine = templateEngine;
 	}
 
 	public void saveOrder(BigDecimal amount, String email) throws JsonProcessingException {
@@ -53,6 +63,54 @@ public class OrderService {
 				.setHeader("CREATED_DATE", Timestamp.valueOf(LocalDateTime.now()))
 				.build();
 
+		mailGateway.enqueueMailEvent(message);
+	}
+
+	public void saveOrder2(String name, BigDecimal amount, String email) throws JsonProcessingException {
+		// create order
+		Order order = new Order();
+
+		// set prop of order
+		order.setAmount(amount);
+		order.setCustomerEmail(email);
+
+		// save order
+		Order savedOrder = orderRepository.save(order);
+
+		// create outbox
+		String uuid = UUID.randomUUID().toString();
+		Outbox outbox = new Outbox(uuid, "ORDER_CREATED", new ObjectMapper().writeValueAsString(savedOrder), "PENDING");
+
+		// save outbox
+		outboxRepository.save(outbox);
+
+		// create context(ctx)
+		Context context = new Context();
+		context.setVariable("name", name);
+		context.setVariable("amount", amount);
+		context.setVariable("email", email);
+		context.setVariable("from", MAIL_USERNAME);
+
+		// process html template through template engine with ctx
+		String htmlStringContent = templateEngine.process("order-success-template", context);
+		System.out.println("======================");
+		System.out.println("Template Engine에 의해 처리된 htmlStringContent: " + htmlStringContent);
+		System.out.println("======================");
+
+		// create MailMessage(order's ID, ctx rendered html string, email)
+		MailMessage mailMessage = new MailMessage(
+				"%s번 주문 처리 성공".formatted(savedOrder.getId()),
+				"%s".formatted(htmlStringContent),
+				email
+		);
+
+		// create message
+		Message<?> message = MessageBuilder.withPayload(mailMessage)
+				.setHeader("MESSAGE_ID", uuid)
+				.setHeader("CREATED_DATE", Timestamp.valueOf(LocalDateTime.now()))
+				.build();
+
+		// enqueue message
 		mailGateway.enqueueMailEvent(message);
 	}
 }
--- src/test/java/com/springboot/springintegrationpostgresqlpushnotification/domain/order/service/OrderServiceTest.java
+++ src/test/java/com/springboot/springintegrationpostgresqlpushnotification/domain/order/service/OrderServiceTest.java
@@ -5,12 +5,15 @@ import com.springboot.springintegrationpostgresqlpushnotification.domain.notific
 import com.springboot.springintegrationpostgresqlpushnotification.domain.order.data.entity.Order;
 import com.springboot.springintegrationpostgresqlpushnotification.domain.order.data.repository.OrderRepository;
 import com.springboot.springintegrationpostgresqlpushnotification.global.util.mail.MailGateway;
+import com.springboot.springintegrationpostgresqlpushnotification.global.util.mail.MailMessage;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.messaging.Message;
+import org.thymeleaf.context.Context;
+import org.thymeleaf.spring6.SpringTemplateEngine;
 
 import java.math.BigDecimal;
 
@@ -32,6 +35,9 @@ public class OrderServiceTest {
 	@MockBean
 	private MailGateway mailGateway;
 
+	@MockBean
+	private SpringTemplateEngine templateEngine;
+
 	@Test
 	@DisplayName("is saved in the repository and the email is sent correctly")
 	public void testSaveOrderSendsEmail() throws JsonProcessingException {
@@ -50,5 +56,33 @@ public class OrderServiceTest {
 
 		verify(orderRepository, times(1)).save(any(Order.class));
 		verify(mailGateway, times(1)).enqueueMailEvent(any(Message.class));
+	}
+
+	@Test
+	@DisplayName("is saved in the repository and the email is sent correctly with rendered template")
+	public void testSaveOrder2SendsEmailWithTemplate() throws JsonProcessingException {
+		BigDecimal amount = BigDecimal.valueOf(200.0);
+		String email = "testSaveOrder2SendsEmailWithTemplate@email.com";
+		String name = "testname";
+		String renderedHtml = "<html><body>주문 성공 본문</body></html>";
+
+		Order order = new Order(amount, email);
+
+		when(orderRepository.save(any(Order.class))).thenReturn(order);
+		when(templateEngine.process(eq("order-success-template"), any(Context.class))).thenReturn(renderedHtml);
+
+		orderService.saveOrder2(name, amount, email);
+
+		verify(outboxRepository, times(1)).save(argThat(outbox ->
+				outbox.getEventType().equals("ORDER_CREATED") &&
+						outbox.getStatus().equals("PENDING") && outbox.getPayload().contains("\"amount\":200.0")
+		));
+
+		verify(orderRepository, times(1)).save(any(Order.class));
+		verify(templateEngine, times(1)).process(eq("order-success-template"), any(Context.class));
+		verify(mailGateway, times(1)).enqueueMailEvent(argThat(message ->
+				message.getPayload() instanceof MailMessage &&
+						((MailMessage) message.getPayload()).body().equals(renderedHtml)
+		));
 	}
 }
