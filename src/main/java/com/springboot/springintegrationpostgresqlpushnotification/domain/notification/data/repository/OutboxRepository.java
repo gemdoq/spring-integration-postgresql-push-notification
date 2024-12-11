4ee8c6f6ab4cec90995a202924111dd9eb82b332
SeungHo <gemdoq@gmail.com>
2024-12-11 16:00:46 +0900

    feat(outbox): add method for delete an oldFailedMessage in repository

--- src/main/java/com/springboot/springintegrationpostgresqlpushnotification/domain/notification/data/repository/OutboxRepository.java
+++ src/main/java/com/springboot/springintegrationpostgresqlpushnotification/domain/notification/data/repository/OutboxRepository.java
@@ -7,12 +7,19 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.sql.Timestamp;
 import java.util.List;
 
 public interface OutboxRepository extends JpaRepository<Outbox, String> {
 	List<Outbox> findByStatus(String status);
+
 	@Modifying
 	@Transactional
 	@Query("UPDATE Outbox o SET o.status = :status WHERE o.id = :messageId")
 	void updateStatus(@Param("messageId") String messageId, @Param("status") String status);
+
+	@Modifying
+	@Transactional
+	@Query("DELETE FROM Outbox o WHERE o.status = 'FAILED' AND o.createdDttm < :timestamp")
+	int deleteOldFailedMessages(@Param("timestamp") Timestamp timestamp);
 }
