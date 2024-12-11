f8785cc2f3e9cc7f652ccb5b56311aae8e4016bb
SeungHo <gemdoq@gmail.com>
2024-12-11 16:08:37 +0900

    chore(import): delete useless import

--- src/test/java/com/springboot/springintegrationpostgresqlpushnotification/global/util/mail/SmtpMailSenderTest.java
+++ src/test/java/com/springboot/springintegrationpostgresqlpushnotification/global/util/mail/SmtpMailSenderTest.java
@@ -8,7 +8,6 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.test.util.ReflectionTestUtils;
 
