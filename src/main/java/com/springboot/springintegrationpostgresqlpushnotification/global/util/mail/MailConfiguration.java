564de5582d240a198916d87110833db00c3d2623
SeungHo <gemdoq@gmail.com>
2024-12-12 22:09:15 +0900

    feat(maillog): add logic to save mail log after sending email

--- src/main/java/com/springboot/springintegrationpostgresqlpushnotification/global/util/mail/MailConfiguration.java
+++ src/main/java/com/springboot/springintegrationpostgresqlpushnotification/global/util/mail/MailConfiguration.java
@@ -1,7 +1,9 @@
 package com.springboot.springintegrationpostgresqlpushnotification.global.util.mail;
 
+import com.springboot.springintegrationpostgresqlpushnotification.domain.maillog.service.MailLogService;
 import com.springboot.springintegrationpostgresqlpushnotification.domain.notification.data.repository.OutboxRepository;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.integration.channel.DirectChannel;
@@ -22,6 +24,9 @@ import java.time.Duration;
 public class MailConfiguration {
 	private final OutboxRepository outboxRepository;
 	private final RequestHandlerRetryAdvice retryAdvice;
+
+	@Value("${MAIL_USERNAME}")
+	private String MAIL_USERNAME;
 
 	public MailConfiguration(
 			OutboxRepository outboxRepository,
@@ -59,7 +64,7 @@ public class MailConfiguration {
 	}
 
 	@Bean
-	public IntegrationFlow mailFlow(JdbcChannelMessageStore store, MailSender mailSender) {
+	public IntegrationFlow mailFlow(JdbcChannelMessageStore store, MailSender mailSender, MailLogService mailLogService) {
 		return IntegrationFlow.from(inbox())
 				.log(LoggingHandler.Level.INFO, message -> "수신된 메시지: " + message)
 				.channel(outbox(store))
@@ -70,12 +75,13 @@ public class MailConfiguration {
 						mailSender.sendMail(mail);	// 메일 전송 시도
 						outboxRepository.updateStatus(messageId, "PROCESSED");	// 성공 시 상태 업데이트
 						System.out.println("메시지가 성공적으로 처리되었습니다" + messageId);
+						mailLogService.saveMailLog(MAIL_USERNAME, mail.to(), mail.subject(), mail.body()); // 메일 전송 성공 시 메일로그 저장
 					} catch (MailSendException e) {
 						System.out.println("메일 전송 에러 발생: " + e.getMessage());
 						// 재시도 실패 시만 상태 업데이트
 						throw new RuntimeException(e);					}
 				}, e -> e.advice(retryAdvice) // 재시도 로직 추가
-						.poller(Pollers.fixedDelay(Duration.ofSeconds(1)).transactional()))
+						.poller(Pollers.fixedDelay(Duration.ofSeconds(1)).maxMessagesPerPoll(10).transactional()))
 				.get();
 	}
 }
